672544712892196f03c52a25c88950481e11fc59
Seweryn <sewcio0212@gmail.com>
2024-08-14 16:30:38 +0200

    firebase added + test-scheduler created

---
+++ src/firebaseConfig.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDDUnXuzhuFDSKjXH45fBXwmbHYfJPPUiA",
+  authDomain: "react-scheduler-wellmarketing.firebaseapp.com",
+  projectId: "react-scheduler-wellmarketing",
+  storageBucket: "react-scheduler-wellmarketing.appspot.com",
+  messagingSenderId: "825732343599",
+  appId: "1:825732343599:web:52abe4659a04857aa1a3da",
+  measurementId: "G-2C9SS4LK5W",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const firestore = getFirestore(app);
